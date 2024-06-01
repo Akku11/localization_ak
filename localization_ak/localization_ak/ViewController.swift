30660d5b09993fcd80d5b2a6cfe6107e01420dbb
Akanksha <>
2024-06-01 10:52:11 +0800

    Hardcoded localization for  german language (de)

--- localization_ak/localization_ak/ViewController.swift
+++ localization_ak/localization_ak/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  @IBOutlet weak var subTitleLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    titleLabel.text = "welcome_title".localised() // <-------------   Hardcoded localisation
+    
+    subTitleLabel.text = String(localized: "login_button")
   }
 
 
